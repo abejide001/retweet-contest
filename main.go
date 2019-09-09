7247ebb8258fc55501f79576e8cebc5f2b565c52
abejide001 <abejidefemi1@gmail.com>
2019-09-09 16:17:41 +0100

    authenticate account

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"context"
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/oauth2"
 )
 
 func main() {
 	var keys struct {
-		Key     string `json:"consumer_key"`
+		Key    string `json:"consumer_key"`
 		Secret string `json:"consumer_secret"`
 	}
 	f, err := os.Open("keys.json")
@@ -17,6 +23,33 @@ func main() {
 	}
 	defer f.Close()
 	dec := json.NewDecoder(f)
-	dec.Decode(&keys) //decodes the string
-	fmt.Println(keys)
+	dec.Decode(&keys) // decodes the string
+	req, err := http.NewRequest("POST", "https://api.twitter.com/oauth2/token", strings.NewReader("grant_type=client_credentials"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.SetBasicAuth(keys.Key, keys.Secret)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+	var client http.Client
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+	var token oauth2.Token
+	dec = json.NewDecoder(res.Body)
+	err = dec.Decode(&token)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var conf oauth2.Config
+	TwitterClient := conf.Client(context.Background(), &token)
+	response, err := TwitterClient.Get("https://api.twitter.com/1.1/statuses/retweets/1171044354359726082.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer response.Body.Close()
+	io.Copy(os.Stdout, response.Body)
 }
